167dfd569b17553a3a3cd06c3f602b2430fc4651
Momi <amekaka@gmail.com>
2023-10-24 16:32:07 +0800

    設置我們 Tailwind CSS 的內容作用範圍

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    // "./index.html", // 因為這裡不會直接對 HTML 加 Class 所以拿掉
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  // ...
+};
