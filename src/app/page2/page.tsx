30f8b862ff4f006b11b9e3a725e8d4dbf43b5ae5
Momi <amekaka@gmail.com>
2023-10-25 16:31:49 +0800

    feat: 調整 Vite 的打包方式
    scripts 中 preview 新增vite build 能夠 build+preview ,vite config 新增 sever:{open:true} 能在 build 之後自動打開
    dist 中的檔案.

---
+++ src/app/page2/page.tsx
@@ -0,0 +1,12 @@
+import pokemon from "../../assets/pokemon.png";
+
+function HomePage() {
+  return (
+    <h1>
+      Page2
+      <img src={pokemon} alt="" />
+    </h1>
+  );
+}
+
+export default HomePage;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,40 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
+
+const pageNames = ["page2"];
+
+const getPageHTML = (rootDir: string, pageName: string) => resolve(rootDir, `${pageName}.html`);
+
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
+  server: {
+    open: true,
+  },
+  build: {
+    assetsDir: "",
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        ...pageNames.reduce((inputConfig: { [key: string]: string }, name) => {
+          // eslint-disable-next-line no-param-reassign
+          inputConfig[`${name}`] = getPageHTML(__dirname, name);
+          return inputConfig;
+        }, {}),
+      },
+      output: {
+        assetFileNames: (fileData) => {
+          const { name } = fileData;
+          const extType = name.split(".").at(-1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            return `assets/[name]-[hash][extname]`;
+          }
+          return `[name]/[name]-[hash][extname]`;
+        },
+        entryFileNames: "[name]/[name]-[hash].js",
+      },
+    },
+  },
 });
